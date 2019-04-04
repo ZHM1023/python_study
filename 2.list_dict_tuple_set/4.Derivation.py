b17b13c5923c25beaf04e9fbcfc57fe9268551db
bavdu <bavduer@163.com>
2019-04-04 09:26:01 +0800

    derivation

---
+++ 2.list_dict_tuple_set/4.Derivation.py
@@ -0,0 +1,19 @@
+# filename: derivation.py
+
+
+
+
+
+chinese_zodiac = [i for i in range(20) if i % 2 == 0]
+# for i in range(10):
+#     if i % 2 == 0:
+#         chinese_zodiac.append(i)
+print(chinese_zodiac)
+
+
+dict_zodiac = {key:0 for key in chinese_zodiac}
+# for key in range(len(chinese_zodiac)):
+#     dict_zodiac[chinese_zodiac[key]] = 0
+print(dict_zodiac)
+
+
