3d5ab0c2a8e7150aea393bd4779365b9251f14ca
bavdu <bavduer@163.com>
2019-04-04 10:25:59 +0800

    functions

--- 3.functions_basis/case_traversing_dict.py
+++ 3.functions_basis/case_traversing_dict.py
@@ -8,18 +8,24 @@ dict01 = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
 # traverdict(dict01)
 
 
-names = ['Michael', 'Bob', 'Tracy']
-scores = [95, 75, 85]
+
 # dict01 = {key:value for key in names for value in scores}
 # print(dict01)
 
 # for i in names:
 #     dict02[i] = scores[]
-dict02 = {}
-i = 0
-while i < len(names):
-    dict02[names[i]] = scores[i]
-    i += 1
-print(dict02)
 
+def createdict(key_list01, value_list02):
+    fdict = {}
+    if len(key_list01) == len(value_list02):
+        i = 0
+        while i < len(key_list01):
+            fdict[key_list01[i]] = value_list02[i]
+            i += 1
+        return fdict
+    else:
+        print("check your list len.")
 
+names = ['Michael', 'Bob', 'Tracy']
+scores = [95, 75, 85]
+print(createdict(names, scores), type(createdict(names, scores)))
