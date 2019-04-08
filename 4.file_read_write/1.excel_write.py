b0b3c2183d5657ddc25c4db84d1d1aa8caaa1a26
bavdu <bavduer@163.com>
2019-04-08 15:14:51 +0800

    file excel

--- 4.file_read_write/1.excel_write.py
+++ 4.file_read_write/1.excel_write.py
@@ -9,7 +9,7 @@
 import xlwt
 
 list01 = [["name", "age", "sex", "score"],
-          ["Tom", 21, "Man", 59.5],
+          ["Tom", 21, "Man", 101],
           ["Jerry", 21, "Woman", 60.5],
           ["Hale", 23, "Man", 79],
           ["Role", 30, "Woman", 100]]
@@ -20,18 +20,30 @@ list01 = [["name", "age", "sex", "score"],
 # list01.append(list02)
 # print(list01, type(list01))
 
-table = xlwt.Workbook(encoding="utf-8")
-sheet = table.add_sheet("sheet01")
-
-row = 0
-for data in list01:
-    col = 0
-    for element in data:
-        sheet.write(row, col, element)
-        col += 1
-    row += 1
-
-table.save("./information.xlsx")
+# table = xlwt.Workbook(encoding="utf-8")
+# sheet = table.add_sheet("sheet01")
+# row = 0
+# for data in list01:
+#     col = 0
+#     for element in data:
+#         sheet.write(row, col, element)
+#         col += 1
+#     row += 1
+# table.save("./information.xlsx")
 
 
+def writable(data, filename):
+    table = xlwt.Workbook(encoding="utf-8")
+    sheet = table.add_sheet("sheet1")
+
+    row = 0
+    for d in data:
+        col = 0
+        for information in d:
+            sheet.write(row, col, information)
+            col += 1
+        row += 1
+    table.save(filename)
+
+writable(list01, "test.xlsx")
 
---
+++ 4.file_read_write/2.excel_read.py
@@ -0,0 +1,4 @@
+# Author: bavdu
+# Email: bavduer@163.com
+# Date: 2019/04/07
+# Usage: study read excel file.
