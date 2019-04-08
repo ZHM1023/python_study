bede20555044cd591cd50635a02a76f3d7289f68
bavdu <bavduer@163.com>
2019-04-08 14:54:14 +0800

    file excel

---
+++ 4.file_read_write/1.excel_write.py
@@ -0,0 +1,37 @@
+# Author: bavdu
+# Email: bavduer.163.com
+# Date: 2019/04/07
+# Usage: study excel write.
+
+
+# pip3 install xlrd xlwt xlutils
+
+import xlwt
+
+list01 = [["name", "age", "sex", "score"],
+          ["Tom", 21, "Man", 59.5],
+          ["Jerry", 21, "Woman", 60.5],
+          ["Hale", 23, "Man", 79],
+          ["Role", 30, "Woman", 100]]
+
+# list01 = [["name", "age", "sex", "score"]]
+# list02 = ["Tom", 21, "Man", 59.5]
+#
+# list01.append(list02)
+# print(list01, type(list01))
+
+table = xlwt.Workbook(encoding="utf-8")
+sheet = table.add_sheet("sheet01")
+
+row = 0
+for data in list01:
+    col = 0
+    for element in data:
+        sheet.write(row, col, element)
+        col += 1
+    row += 1
+
+table.save("./information.xlsx")
+
+
+
